92edf73d0b2fefe1dc101fec087fa6601ffb6a7d
Denys <FightForDobro@gmail.com>
2019-08-18 00:32:30 +0300

    Add second homework and minor fix to black jack game

---
+++ Ushakov_Denys/02/Task_2.py
@@ -0,0 +1,15 @@
+# Normal string
+input_str = input('Enter something -->')
+
+# Reversed string
+r_str = ''
+
+for i in input_str:
+    r_str = i + r_str  # add symbols to empty string
+
+print(f'Normal string: {input_str}')
+print(f'Reversed string: {r_str}')
+
+
+
+

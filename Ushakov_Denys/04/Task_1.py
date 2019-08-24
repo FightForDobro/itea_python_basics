0e8fa39fa27d56626d3762a14a0e9cdab5829496
Denys <FightForDobro@gmail.com>
2019-08-25 01:21:03 +0300

    Allmost done Text Analyzer
    remains only docstrings and comments

--- Ushakov_Denys/04/Task_1.py
+++ Ushakov_Denys/04/Task_1.py
@@ -5,29 +5,49 @@ def calculate_words_in_text(text):
 
     return len(text)
 
+
 def find_unique_words(text):
 
-    a = set(text)
-
-    return a
+    return set(text)
 
 
-def find_frequent_words(text): # Закончить сортировку словаря
+def find_frequent_words(s_dict):
 
-    count_num_dict = {}
-    for i in text:
-        count_num_dict[i] = text.count(i)
-
-    value_list = []
-    for key, value in count_num_dict.items():
-        pass
-
-    return count_num_dict.items()
+    return sorted(s_dict.items(), key=lambda x: x[1], reverse=True)
 
 
-def calculate_frequency_of_all_world(text):
+def adopt_word_in_dict(text):
 
-    pass
+    word_dict = {}
+
+    for word in text:
+
+        if word not in word_dict.keys():
+
+            word_dict[word] = 1
+
+        else:
+
+            word_dict[word] += 1
+
+    return word_dict
+
+
+def calculate_frequency_of_all_world(s_dict):
+
+    freq_list = []
+    freq_text = ''
+
+    for key, value in s_dict.items():
+
+        s_dict[key] = int(value / word_quantity * 100)
+
+    for key, value in s_dict.items():
+
+        freq_list.append(f'{str(value)}% ')
+        freq_list.append(f'{key} = ')
+
+    return freq_text.join(reversed(freq_list))
 
 
 def adapts_text_to_correct_format(text):
@@ -48,30 +68,40 @@ def adapts_text_to_correct_format(text):
                 index = text.index(i)
 
                 if j == i:
+
                     text.pop(index)
+
                     break
 
     return text
 
 
-main_text = '''On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.'''
-
-test_text = 'the the the the the the the'
+main_text = 'hello my name is denys name denys denys'
 
 forbidden_punctuation = ['.', ',', '?', '!', "'", '"', ':', ';', '...', '-', '–', '—', '(', ')', '[', ']']
 forbidden_connectives = ['and', 'so', 'or', 'in', 'to', 'at', 'on', 'a', 'an', 'to', 'is', 'are', 'was', 'were', 'will', 'would', 'could', 'if', 'he', 'she', 'it', 'this', 'my', 'of', 'we', 'who', 'by', 'the', 'so', 'that', 'they', 'as', 'our', 'when', 'which', 'do', 'be', 'but', 'else', 'have', 'with']
 
-test_f_c = ['a', 'the', 'and', 'so']
-
 adapted_text = adapts_text_to_correct_format(main_text)
+
+adapted_dict = adopt_word_in_dict(adapted_text)
 
 word_quantity = calculate_words_in_text(adapted_text)
 
 unique_words = find_unique_words(adapted_text)
 
-keyword = find_frequent_words(adapted_text)
+all_word_freq = calculate_frequency_of_all_world(adapted_dict)
 
-print(adapted_text)
-print(f'In your text: {word_quantity} words')
-print(f'In your text you have unique word: {len(unique_words)} your unique words is {unique_words}')  # Убрать {}
-print(f'{keyword}')
+keyword = find_frequent_words(adapted_dict)
+
+a = [word for word in unique_words]
+
+print(f'In your text: {word_quantity} words\n')
+
+print(f'In your text you have {len(unique_words)} unique words, your unique words is: {a[0]}, {a[1]}, {a[2]}\n')
+
+print(f'      Top 3 word:\n'
+      f'1: {keyword[0][0]} appears {keyword[0][1]} times\n'
+      f'2: {keyword[1][0]} appears {keyword[1][1]} times\n'
+      f'3: {keyword[2][0]} appears {keyword[2][1]} times\n')
+
+print(f'frequency: {all_word_freq}')

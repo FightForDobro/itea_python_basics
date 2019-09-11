d9a76b4b9912c6b16a150fe58371163ccb916528
Dima <l0635968488@gmail.com>
2019-09-11 13:16:16 +0300

    HW6

---
+++ pavlenko_dmitryi/06/enemies.py
@@ -0,0 +1,71 @@
+class Enemy:
+
+    def __init__(self):
+
+        self._hp = 10
+        self._damage = 20
+
+    def attack(self):
+
+        return self._damage
+
+    def take_damage(self, damage):
+
+        self._hp -= damage
+        is_dead = self.is_dead()
+
+        if is_dead:
+            print(self.on_death())
+            return is_dead
+
+        print(f'Enemy hp: {self._hp}')
+
+    def is_dead(self):
+
+        if self._hp <= 0:
+            return True
+
+    def on_death(self):
+
+        print('I am dead')
+
+
+class Murloc(Enemy):
+
+    def __init__(self):
+
+        print('I am murloc')
+        super().__init__()
+        self._hp *= 0.5
+        self._damage *= 0.5
+
+    def on_death(self):
+        return 'Mrglglglg'
+
+
+class Undead(Enemy):
+
+    def __init__(self):
+
+        print('I am undead')
+        super().__init__()
+        self._hp *= 1.2
+        self._damage *= 1.1
+
+    def on_death(self):
+        return 'Uuuuuh'
+
+class Ogr (Enemy):
+
+    def __init__(self):
+
+        print('I am Ogr')
+        super().__init__()
+        self._hp *= 1.8
+        self._damage *= 0.9
+
+    def on_death(self):
+        return 'Orrrr'
+
+
+ENEMIES_TYPES = [Murloc, Undead, Ogr]
